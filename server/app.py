bf1270d7361bea2ac78c38abf5cc0b34a82a9969
Faizaan Vidhani <faizaanvidhani@gmail.com>
2023-01-06 14:38:48 -0600

    feat: created client and server directories

---
+++ server/app.py
@@ -0,0 +1,124 @@
+"""
+Accessing data on the Spotify platform via Spotipy, a Python library for the Spotify Web API.
+https://spotipy.readthedocs.io/en/2.22.0/#spotipy.client.Spotify.auth_manager
+"""
+from flask import Flask, request, url_for, session, redirect, jsonify
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import time
+from recommender import Recommender
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(12).hex()
+app.config['SESSION_COOKIE_NAME'] = "User's cookie"
+TOKEN_INFO = "token_info"
+
+recommender = Recommender()
+load_dotenv()
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=url_for('callback', _external=True),
+        scope="user-top-read")
+
+@app.route('/')
+def index():
+    return 'home'
+
+@app.route('/login')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return jsonify(auth_url)
+
+
+@app.route('/callback')
+def callback():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    print(code)
+    token_info = sp_oauth.get_access_token()
+    # token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect("http://localhost:3000/#/top-tracks")
+    # return redirect(url_for('getTracks', _external=True))
+
+@app.route('/getTracks')
+def getTracks():
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        return redirect("/")
+
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+    results = recommender.get_user_top_tracks(sp)
+    return results
+
+@app.route('/logout')
+def logout():
+    session.pop(TOKEN_INFO, None)
+    session.clear()
+
+    # if os.path.exists('./.cache'):
+    #     os.remove('./.cache')  
+    print("removed cache file")
+    return "done"
+    # return redirect("http://www.accounts.spotify.com/logout")
+
+@app.route('/search/<string:country>')
+def search(country):
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        return redirect("/")
+    
+
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+
+    user_tracks_scores = recommender.get_user_top_tracks(sp)
+    country_tracks_scores = recommender.get_country_tracks(sp, country)
+    sorted_distances = recommender.calculate_euclidean_distance(user_tracks_scores, country_tracks_scores)
+    return jsonify(sorted_distances)
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise Exception("ERROR: Null token")
+    
+    curr_time = int(time.time())
+    is_expired = token_info['expires_at'] - curr_time < 60
+    if (is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+
+    return token_info
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+    
+
+
+
+
+
---
+++ server/recommender.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+class Recommender:
+    """
+    Recommender class. Contains methods for finding tracks from new album releases in a particular 
+    country similar to a given user's top tracks.
+    """
+
+    def main(self, sp):
+        """
+        Executes logic for finding tracks from new album releases in a particular country similar to a
+        user's top tracks. Returns dictionary mapping track IDs to distances from user's top tracks in 
+        ascending order.
+        """
+        top_tracks_ids, top_tracks_info = sp.current_user_top_tracks(limit=10)
+        user_tracks_scores = self.compute_user_scores(sp, top_tracks_ids, top_tracks_info)
+
+        country_tracks_ids, country_tracks_info = country_tracks_info = self.get_country_tracks(sp)
+        country_tracks_scores = self.compute_country_scores(sp, country_tracks_ids, country_tracks_info)
+
+        sorted_distances = self.calculate_euclidean_distance(user_tracks_scores, country_tracks_scores)
+        return sorted_distances
+
+
+    def get_user_top_tracks(self, sp):
+        """
+        Returns the following:
+        1) list of track IDs for current top 10 tracks for a particular user
+        2) list of dictionaries, each containing the ranking, track id, title, artist, image, 
+        preview_url, and audio features for a particular track.
+        """
+        user_tracks_info = sp.current_user_top_tracks(limit=10)
+
+        top_tracks_info = []
+        track_ranking = 0
+        for track in user_tracks_info["items"]:
+            track_info = {}
+            track_ranking += 1
+            track_info["ranking"] = track_ranking
+            track_info["id"] = track["id"]
+            track_info["title"] = track["name"]
+            track_info["artist"] = track["album"]["artists"][0]["name"]
+            track_info["image"] = track["album"]["images"][0]["url"]
+            track_info["preview_url"] = track["preview_url"]
+            track_info["features"] = self.find_audio_features(sp, track["id"])
+            top_tracks_info.append(track_info)
+        return top_tracks_info
+
+    def get_country_tracks(self, sp, country_code):
+        """
+        Returns the following:
+        1) list of track IDs for top 20 latest album releases for a given country
+        2) list of dictionaries, each containing the track id, title, artist, image, 
+        preview_url, and audio features for a particular track.
+        """
+        album_releases = sp.new_releases(country_code, limit=10)["albums"]["items"]
+        country_tracks_info = []
+        for album in album_releases:
+            tracks = sp.album_tracks(album["id"], limit=50)["items"]
+            for track in tracks:
+                track_info = {}
+                track_info["id"] = track["id"]
+                track_info["title"] = track["name"]
+                track_info["artist"] = track["artists"][0]["name"]
+                track_info["image"] = album["images"][0]["url"]
+                track_info["preview_url"] = track["preview_url"]
+                track_info["features"] = self.find_audio_features(sp, track["id"])
+                country_tracks_info.append(track_info)
+
+        return country_tracks_info
+
+
+
+    def find_audio_features(self, sp, track_id):
+        """
+        Computes the danceability, energy, speechiness, acousticness, instrumentalness, liveness, and 
+        valence scores for a list of tracks. Populates each dictionary in the tracks_info list with a
+        mapping of 'features' to a dictionary of the audio features for that particular track. 
+        """
+        feats = sp.audio_features(track_id)[0]
+        return {
+                "danceability": feats['danceability'],
+                "energy": feats['energy'],
+                "speechiness": feats['speechiness'],
+                "acousticness": feats['acousticness'],
+                "instrumentalness": feats['instrumentalness'],
+                "liveness": feats["liveness"],
+                "valence": feats["valence"]
+            }
+    
+    
+    def calculate_euclidean_distance(self, user_tracks_info, country_tracks_info):
+        """
+        Returns mapping of track IDs to distances from user's top track preferences in
+        ascending order.
+        """
+
+        for i in range(len(country_tracks_info)):
+            dist = 0
+            point_1 = np.array(list(country_tracks_info[i]["features"].values()))
+            for j in range(len(user_tracks_info)):
+                point_2 = np.array(list(user_tracks_info[j]["features"].values()))
+                dist += np.linalg.norm(point_1 - point_2)
+            country_tracks_info[i]["distance"] = dist
+        sorted_country_tracks = sorted(country_tracks_info, key=lambda d:d["distance"])
+        return sorted_country_tracks
+
+
+
+
+    
+    
+
+
+
+    
+
+
+
+
+
